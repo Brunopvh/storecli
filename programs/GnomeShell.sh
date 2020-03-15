70db3734b0ded5d105525ba2308fe9ae7b392d58
Brunopvh <brunodasill@gmail.com>
2020-03-15 16:52:31 -0400

    melhorias

--- programs/GnomeShell.sh
+++ programs/GnomeShell.sh
@@ -12,34 +12,55 @@ function _gnome_shell()
 		return 1
 	fi
 
+	# Fedora
+	local array_gnome_shell_fedora=(
+	'gnome-tweaks' 'gnome-shell-extension-topicons-plus'
+	'gnome-shell-extension-drive-menu' 'gnome-backgrounds-extras'
+	'verne-backgrounds-gnome'
+	)
+
+	# ArchLinux
+	local array_gnome_shell_arclinux=(
+	'gnome-tweaks' 'gnome-backgrounds'
+	)
+
 	#----------------------------------------------------#
 	# Verifcar qual o gerenciador de pacotes da distro e instalar pacotes 
 	# e extenções do gnome-shell.
 	#----------------------------------------------------#
-	echo -e "$space_line"
-	_white "Instalando os pacotes: ${array_gnome_shell[@]}"
-
-	for c in "${array_gnome_shell[@]}"; do
-
-		echo -e "$space_line"
-		_white "Instalando: [$c]"
-
-		if _WHICH "dnf"; then            # RedHat
+	
+	if _WHICH "dnf"; then            # RedHat
+		for c in "${array_gnome_shell_fedora[@]}"; do
+			echo -e "$space_line"
+			_white "Instalando: $c"
 			sudo dnf install -y "$c"
+		done
 
-		elif _WHICH "zypper"; then       # Suse
+	elif _WHICH "zypper"; then       # Suse
+		for c in "${array_gnome_shell_fedora[@]}"; do
+			echo -e "$space_line"
+			_white "Instalando: $c"
 			sudo zypper install -y "$c"
+		done
 
-		elif _WHICH "pacman"; then       # ArchLinux
+	elif _WHICH "pacman"; then       # ArchLinux
+		for c in "${array_gnome_shell_arclinux[@]}"; do
+			echo -e "$space_line"
+			_white "Instalando: $c"
 			sudo pacman -S "$c"
+		done
 
-		elif _WHICH "apt"; then          # Debian
+	elif _WHICH "apt"; then          # Debian
+		for c in "${array_gnome_shell_debian[@]}"; do
+			echo -e "$space_line"
+			_white "Instalando: $c"
 			sudo apt install -y "$c"
+		done
 
-		else
-			msg "Falha"
-			exit 1
-			break
-		fi
-	done
+	else
+		msg "Falha"
+		exit 1
+		break
+	fi
+
 }
