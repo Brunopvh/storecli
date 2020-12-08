37286f66919e4748e8b1b6ad6efcb96766f90fd8
Brunopvh <brunodasill@gmail.com>
2020-12-08 17:33:59 -0400

    Melhorar o insalador

--- setup.sh
+++ setup.sh
@@ -12,201 +12,145 @@ __version__='2020-12-08'
 # sh -c "$(curl -fsSL https://raw.github.com/Brunopvh/storecli/master/setup.sh)"
 #
 
+URL_STORECLI_MASTER='https://github.com/Brunopvh/storecli/archive/master.tar.gz'
+TEMP_DIR="$(mktemp --directory)-installer-storecli"
+UNPACK_DIR="$TEMP_DIR/unpack"
+mkdir -p "$UNPACK_DIR"
 
-Red="\033[0;31m"
-Green="\033[0;32m"
-CBGreen='\033[5;32m'
-Yellow="\033[0;33m"
-White="\033[0;37m"
-Reset="\033[0m"
-
-_msg()
-{
-	echo "$@"
-}
-
-_red()
-{
-	echo "$(printf $Red)$@$(printf $Reset)"
-}
-
-_green()
-{
-	echo "$(printf $Green)$@$(printf $Reset)"
-}
-
-_yellow()
-{
-	echo "$(printf $Yellow)$@$(printf $Reset)"
-}
-
-# URLs
-github='https://github.com'
-url_proj="$github/Brunopvh/storecli"
-url_repo="${url_proj}.git"
-url_master="${url_proj}/archive/master.tar.gz"
-
-
-
-# Definir o destino de instalação do script storecli, se o usuário for 'root'
-# a instalação será em /opt com link simbolico em /usr/local/bin/storecli - se 
-# não, o destino de instalação será na HOME do usuário em ~/.local/bin/storecli-amd64
-# com link simbolico em ~/.local/bin/storecli
-
-if [ $(id -u) -eq 0 ]; then
-	destination_storecli='/opt/storecli-amd64'
-	destination_link_storecli='/usr/local/bin/storecli'
+if [ `id -u` -eq 0 ]; then
+	INSTALATION_DIR='/opt/storecli-amd64'
+	DESTINATION_LINK='/usr/local/bin/storecli'
 else
-	destination_storecli=~/".local/bin/storecli-amd64"
-	destination_link_storecli=~/".local/bin/storecli"
+	INSTALATION_DIR=~/.local/bin/storecli-amd64
+	DESTINATION_LINK=~/.local/bin/storecli
 fi
 
-dir_temp=$(mktemp --directory) || dir_temp="/tmp/$USER/update_storecli"
-dir_unpack="$dir_temp/unpack"
-storecli_tar_file="$dir_temp/storecli.tar.gz"
-mkdir -p "$dir_temp"
-mkdir -p "$dir_unpack"
-
-#----------------------------------------------------------#
-
-is_executable()
+_download_storecli()
 {
-	# Verificar se uma executável existe na variável PATH.
-	# retorna 0 se sim, ou 1 se não existir.
-	executable_app=$(command -v $1 2> /dev/null)
-	if [ -x $executable_app ]; then
-		return 0
+	printf "Conectando ... $URL_STORECLI_MASTER "
+	if [ -x $(command -v aria2c 2> /dev/null) ]; then
+		aria2c "$URL_STORECLI_MASTER" -d "$TEMP_DIR" -o storecli.tar.gz 1> /dev/null
+	elif [ -x $(command -v wget 2> /dev/null) ]; then
+		wget -q "$URL_STORECLI_MASTER" -O "$TEMP_DIR"/storecli.tar.gz
+	elif [ -x $(command -v curl 2> /dev/null) ]; then
+		curl -sSL -o "$TEMP_DIR"/storecli.tar.gz
 	else
+		printf "ERRO: Instale uma ferramenta para gerenciar downloads curl|aria2c|wget\n"
 		return 1
 	fi
+	
+	[ $? -eq 0 ] && {
+		printf "OK\n"
+		return 0
+	}
+	printf "ERRO: Falha no download\n"
+	return 1
 }
 
-#----------------------------------------------------------#
-
-if ! is_executable curl && ! is_executable wget && ! is_excutable aria2c; then
-	_red "Instale uma das ferramentas a seguir ... curl, wget ou aria2c"
-	exit 1
-fi
-
-_download_repo()
+_copy_files()
 {
-	# Usar a ferramenta curl, wget ou aria2c para baixar o pacote de instalação.
-	printf "Baixando ... $url_master "
-	if command -v aria2c 2> /dev/null; then
-		aria2c "$url_master" -d "$dir_temp" -o "storecli.tar.gz" 1> /dev/null || {
-		_red "Falha no download"
+	# Libs
+	printf "Copiando lib ==> $INSTALATION_DIR/lib "
+	if cp -R -u lib $INSTALATION_DIR; then
+		printf "OK\n"
+	else
+		printf "ERRO"
 		return 1
-        }
-	elif command -v wget 2> /dev/null; then
-        wget -q "$url_master" -O "$storecli_tar_file" || {
-		_red "Falha no download"
-		return 1
-        }
-    elif command -v curl 2> /dev/null; then
-	    curl -sSL "$url_master" -o "$storecli_tar_file" || {
-		_red "Falha no download"
-		return 1
-        }
 	fi
-	_msg "OK"
+
+	# Scripts
+	printf "Copiando scripts ==> $INSTALATION_DIR/scripts "
+	if cp -R -u scripts $INSTALATION_DIR; then
+		printf "OK\n"
+	else
+		printf "ERRO"
+		return 1
+	fi
+
+	# Stable
+	printf "Copiando stable ==> $INSTALATION_DIR/stable "
+	if cp -R -u stable $INSTALATION_DIR; then
+		printf "OK\n"
+	else
+		printf "ERRO"
+		return 1
+	fi
+
+	# Setup
+	printf "Copiando setup.sh ==> $INSTALATION_DIR/setup.sh "
+	if cp -R -u setup.sh $INSTALATION_DIR; then
+		printf "OK\n"
+	else
+		printf "ERRO"
+		return 1
+	fi
+
+	# Storecli
+	printf "Copiando storecli.sh ==> $INSTALATION_DIR/storecli.sh "
+	if cp -R -u storecli.sh $INSTALATION_DIR; then
+		printf "OK\n"
+	else
+		printf "ERRO"
+		return 1
+	fi
 	return 0
+	# cp -R -v -u storecli-amd64 $INSTALATION_DIR
 }
 
-
-__rmdir__()
+_install_storecli()
 {
-	# Remove um arquivo ou diretório informado no parametro $1
-	# $1 = arquivo/diretório
-	while [ $1 ]; do
-		printf "Removendo ... $1 "
-		if rm -rf "$1"; then
-			_yellow "OK"
-		else
-			_red "Falha"
-		fi
-		shift
-	done
-}
-
-_uninstall()
-{
-	# Desintalar o script storecli.
-	[ -d "$destination_storecli" ] && __rmdir__ "$destination_storecli"
-	[ -L "$destination_link_storecli" ] && __rmdir__ "$destination_link_storecli"
-	return 0
-}
-
-
-_unpack()
-{
-	[ -z $1 ] && return 1
-	local path_file="$1"
-
-	# Limpar o diretório antes da descompressão.
-	cd "$dir_unpack" || return 1
-	__rmdir__ $(ls)
+	_download_storecli || return 1
 	
-	# Descomprimir
-	printf "Descomprimindo: $path_file "
-	tar -zxvf "$path_file" -C "$dir_unpack" 1> /dev/null
-	if [ $? -eq 0 ]; then
-		_yellow "OK"
+	printf "Entrando no diretório ... $TEMP_DIR\n"
+	cd "$TEMP_DIR"
+	printf "Descomprimindo ... storecli.tar.gz "
+	if tar -zxvf storecli.tar.gz -C $UNPACK_DIR 1> /dev/null; then
+		printf "OK\n"
+	else
+		printf "ERRO: Falha na descompressão\n"
+		return 1
+	fi
+
+	cd $UNPACK_DIR
+	mv $(ls -d storecli-*) storecli-amd64
+	! cd storecli-amd64 && {
+		printf "ERRO: diretório 'storecli-amd64' não encontrado\n"
+		return 1
+	}
+
+	mkdir -p $INSTALATION_DIR
+	_copy_files || return 1
+	chmod -R a+x $INSTALATION_DIR
+	ln -sf $INSTALATION_DIR/storecli.sh $DESTINATION_LINK
+
+
+	if [ -x $DESTINATION_LINK ]; then
+		printf "\033[0;33mstorecli instalado com sucesso!\033[m\n"
 		return 0
 	else
-		_red "(_unpack): Erro"
-		__rmdir__ "$path_file"
+		printf "ERRO: Falha na instalação, tente novamente."
 		return 1
 	fi
 }
 
-_install()
-{
-	if [ -z "$destination_storecli" ]; then
-		_red "(_install): 'destination_storecli' tem valor nulo."
-		return 1
-	fi
-	mkdir -p "$destination_storecli"
-
-	printf "Instalando em: $destination_storecli "
-	cd "$dir_unpack" || return 1
-	mv $(ls -d storecli*) storecli 
-	cd storecli
-	cp -R -u * "$destination_storecli"/. || return 1
-	ln -sf "$destination_storecli"/storecli.sh "$destination_link_storecli" || return 1
-	
-	chmod -R a+x "$destination_storecli"
-	chmod a+x "$destination_link_storecli" 
-	
-	if is_executable 'storecli' || [ -x "$HOME/.local/bin/storecli" ] || [ -x "/usr/local/bin/storecli" ]; then
-		_yellow "OK"
-		return 0
-	else
-		_red "(_install): Falha"
-		return 1
-	fi
-}
 
 main()
 {
-	case "$1" in
-		-r|--remove) _uninstall; return 0;;
-	esac
-
-	_download_repo || return 1
-	_uninstall || return 1
-	_unpack "$storecli_tar_file" || return 1
-	_install || return 1
 	
-	_yellow "Execute ... storecli --help"
-
+	case "$1" in 
+		--remove) 
+				printf "Desinstalando storecli ... "
+				rm -rf $INSTALATION_DIR
+				rm -rf $DESTINATION_LINK
+				printf "OK\n"
+				return
+				;;
+	esac			
+	_install_storecli || return 1
+	printf "Limpando arquivos temporários "
+	rm -rf "$TEMP_DIR"
+	printf "OK\n"
 }
 
 main "$@" || exit 1
-
-if [ -d "$dir_temp" ]; then 
-	__rmdir__ "$dir_temp" 1> /dev/null
-fi
 exit 0
-
-
-
