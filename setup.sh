fd34b98289aafc19df9640320d534d3cbcc87917
Brunopvh <brunodasill@gmail.com>
2021-03-02 15:06:29 -0400

    Usar o repositório development como fonte principal para os arquivos da branch development

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@
 # OBS: seu computador precisa estar conectado a internet para executar este instalador.
 # 
 
-__version__='2021-03-01'
+__version__='2021-03-02'
 __script__=$(readlink -f "$0")
 dir_of_project=$(dirname "$__script__")
 
@@ -42,8 +42,8 @@ EOF
 URL_ARCHIVE='https://github.com/Brunopvh/storecli/archive'
 URL_TARFILE_MASTER="$URL_ARCHIVE/master.tar.gz"
 URL_TARFILE_DEVELOPMENT="$URL_ARCHIVE/development.tar.gz"
-URL_SETUP_BASH_LIBS='https://raw.github.com/Brunopvh/bash-libs/main/setup.sh'
-URL_SRC="$URL_TARFILE_MASTER"
+URL_SETUP_BASH_LIBS='https://raw.github.com/Brunopvh/bash-libs/development/setup.sh'
+URL_SRC="$URL_TARFILE_DEVELOPMENT"
 
 clienteDownloader=''
 
