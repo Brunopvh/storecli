b19db743e6d76fa01f9510336e0f6125c57b4885
bschaves <brunodasill@gmail.com>
2022-05-12 09:31:29 -0400

    melhorias

--- lib_info/wine.sh
+++ lib_info/wine.sh
@@ -79,12 +79,21 @@ function _install_wine_base_debian(){
     #
 
     # Verificar versão
-    if [[ $ID == 'debian' ]] || [[ "$VERSION_ID" == 10 ]]; then
+    if [[ $ID == 'debian' ]]; then 
+        if [[ "$VERSION_ID" == 10 ]]; then
         _install_wine_debian_10
-    elif [[ $ID == 'debian' ]] || [[ "$VERSION_ID" == 11 ]]; then
+        elif [[ "$VERSION_ID" == 11 ]]; then
         _install_wine_debian_11
-    elif [[ $ID == 'linuxmint' ]] || [[ "$VERSION_ID" == 5 ]]; then
-        _install_wine_debian_11
+        fi
+
+    elif [[ $ID == 'linuxmint' ]]; then 
+        if [[ $ID_LIKE == 'debian' ]]; then 
+            if [[ "$VERSION_ID" == 5 ]]; then
+                _install_wine_debian_11
+            elif [[ "$VERSION_ID" == 4 ]]; then
+                _install_wine_debian_10
+            fi
+        fi
     else
         printErro "Sistema não suportado"
         return 1
